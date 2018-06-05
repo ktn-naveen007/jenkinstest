36f9cfaf376cd25e1e1026515daf5de46fc9186c
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2018-06-06 00:52:14 +0530

    auto

---
+++ src/main/java/com/ci/JenkinTest/Addition.java
@@ -0,0 +1,12 @@
+package com.ci.JenkinTest;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(10+20);
+
+	}
+
+}
